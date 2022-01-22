c144b6a29806fca087841a7537f7deea67ccd407
kingbri <bdashore3@gmail.com>
2022-01-22 15:53:14 -0500

    Initial Commit

---
+++ OpenInYT/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  OpenInYT
+//
+//  Created by Brian Dashore on 1/22/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ OpenInYT/OpenInYTApp.swift
@@ -0,0 +1,17 @@
+//
+//  OpenInYTApp.swift
+//  OpenInYT
+//
+//  Created by Brian Dashore on 1/22/22.
+//
+
+import SwiftUI
+
+@main
+struct OpenInYTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
